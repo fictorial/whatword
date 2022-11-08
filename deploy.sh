f852e867202decfde70719026305d2bb0c359627
Brian Hammond <brian@caresense.com>
2022-11-08 15:43:38 -0500

    NGINX conf is validated first so we must add rate limiting after

--- deploy.sh
+++ deploy.sh
@@ -17,12 +17,35 @@ while ! dokku enter $APP web cat $PENDING >/dev/null
 do
   sleep 1
 done
+
+# This will not validate on push since we don't have the limit_req_zone in place yet.
+# That has to come after deployment.
+
+rm /home/dokku/$APP/nginx.conf.d/rate_limit.conf
 EOF
 
 git push dokku $DEPLOY_BRANCH
 
-ssh whatword.wtf <<EOF
-perl -pi -e 's/keepalive_timeout\s+\d+/keepalive_timeout 120/' /home/dokku/$APP/nginx.conf
+NGINX_CONF=/home/dokku/$APP/nginx.conf
+NGINX_RATE_LIMIT_CONF=/home/dokku/$APP/nginx.conf.d/rate_limit.conf
 
+ssh whatword.wtf <<EOF
+echo "updating keepalive_timeout"
+perl -pi -e 's/keepalive_timeout\s+\d+/keepalive_timeout 120/' $NGINX_CONF
+
+echo "updating NGINX config for rate limiting: $NGINX_CONF"
+echo 'limit_req_zone \$binary_remote_addr zone=limitreqsbyaddr:20m rate=1r/s;' >/tmp/.conf
+echo 'limit_req_status 429;' >>/tmp/.conf
+echo '' >>/tmp/.conf
+cat $NGINX_CONF >>/tmp/.conf
+mv /tmp/.conf $NGINX_CONF
+
+echo "adding rate limit conf: $NGINX_RATE_LIMIT_CONF"
+echo 'location /guesses { limit_req zone=limitreqsbyaddr; }' >$NGINX_RATE_LIMIT_CONF
+echo 'location /settings { limit_req zone=limitreqsbyaddr; }' >>$NGINX_RATE_LIMIT_CONF
+
+echo "reloading nginx"
 service nginx reload
 EOF
+
+echo done
